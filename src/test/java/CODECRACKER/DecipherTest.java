460c6fe6f5c7116315c5df437d7dd00b9f312939
Stefanov, Tihomir (k20051726) <tihomir.stefanov@kcl.ac.uk>
2021-07-08 11:30:17 +0300

    Initial commit

---
+++ src/test/java/CODECRACKER/DecipherTest.java
@@ -0,0 +1,59 @@
+package CODECRACKER;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the Codecracker program.
+ */
+public class DecipherTest
+{
+    Decipher newWord;
+
+    @BeforeEach
+    public void setUp()
+    {
+        newWord = new Decipher();
+    }
+
+    @Test
+    public void normalInput()
+    {
+        String decryptedWord = "!£o";
+        decryptedWord = newWord.decrypt(decryptedWord);
+        Assertions.assertEquals(decryptedWord,"aez");
+    }
+
+    @Test
+    public void inputWithSpaces()
+    {
+        String decryptedWord = "!£ o";
+        decryptedWord = newWord.decrypt(decryptedWord);
+        Assertions.assertEquals(decryptedWord,"ae z");
+    }
+
+    @Test
+    public void boundaryTest()
+    {
+        String decryptedWord = "!o";
+        decryptedWord = newWord.decrypt(decryptedWord);
+        Assertions.assertEquals(decryptedWord,"az");
+    }
+
+    @Test
+    public void invalidInputTest()
+    {
+        String decryptedWord = "pp";
+        decryptedWord = newWord.decrypt(decryptedWord);
+        Assertions.assertEquals(decryptedWord,"This word cannot be translated because the symbol: p is not part of the encrypted alphabet!");
+    }
+
+    @Test
+    public void blankSpaceTest()
+    {
+        String decryptedWord = "";
+        decryptedWord = newWord.decrypt(decryptedWord);
+        Assertions.assertEquals(decryptedWord,"Please enter a word longer than 0 characters!");
+    }
+}
