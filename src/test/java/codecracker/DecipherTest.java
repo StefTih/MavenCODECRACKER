5d28a2c17e3f7d2d5f243378d3b28782e57dae97
Stefanov, Tihomir (k20051726) <tihomir.stefanov@kcl.ac.uk>
2021-08-16 10:23:37 +0300

    Java conventions applied!

--- src/test/java/codecracker/DecipherTest.java
+++ src/test/java/codecracker/DecipherTest.java
@@ -1,4 +1,4 @@
-package CODECRACKER;
+package codecracker;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,53 +7,46 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit tests for the Codecracker program.
  */
-public class DecipherTest
-{
+public class DecipherTest {
     Decipher newWord;
 
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         newWord = new Decipher();
     }
 
     @Test
-    public void normalInput()
-    {
+    public void normalInput() {
         String decryptedWord = "!£o";
         decryptedWord = newWord.decrypt(decryptedWord);
-        Assertions.assertEquals(decryptedWord,"aez");
+        Assertions.assertEquals(decryptedWord, "aez");
     }
 
     @Test
-    public void inputWithSpaces()
-    {
+    public void inputWithSpaces() {
         String decryptedWord = "!£ o";
         decryptedWord = newWord.decrypt(decryptedWord);
-        Assertions.assertEquals(decryptedWord,"ae z");
+        Assertions.assertEquals(decryptedWord, "ae z");
     }
 
     @Test
-    public void boundaryTest()
-    {
+    public void boundaryTest() {
         String decryptedWord = "!o";
         decryptedWord = newWord.decrypt(decryptedWord);
-        Assertions.assertEquals(decryptedWord,"az");
+        Assertions.assertEquals(decryptedWord, "az");
     }
 
     @Test
-    public void invalidInputTest()
-    {
+    public void invalidInputTest() {
         String decryptedWord = "pp";
         decryptedWord = newWord.decrypt(decryptedWord);
-        Assertions.assertEquals(decryptedWord,"This word cannot be translated because the symbol: p is not part of the encrypted alphabet!");
+        Assertions.assertEquals(decryptedWord, "This word cannot be translated because the symbol: p is not part of the encrypted alphabet!");
     }
 
     @Test
-    public void blankSpaceTest()
-    {
+    public void blankSpaceTest() {
         String decryptedWord = "";
         decryptedWord = newWord.decrypt(decryptedWord);
-        Assertions.assertEquals(decryptedWord,"Please enter a word longer than 0 characters!");
+        Assertions.assertEquals(decryptedWord, "Please enter a word longer than 0 characters!");
     }
 }
